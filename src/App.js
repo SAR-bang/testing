8aee1a2d3e54a1c6a8a72ad8501a38ad959c416e
Adarsha Regmi <adarsharegmi121@gmail.com>
2021-08-21 23:10:14 +0545

    change

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+import Adarsha from "./components/Adarsha";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900">
-      <Navbar />
-      <About />
+      {/* <Navbar /> */}
+      {/* <About />
       <Projects />
       <Skills />
       <Testimonials />
-      <Contact />
+      <Contact /> */}
+      <Adarsha />
     </main>
   );
 }
---
+++ src/components/Adarsha.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Adarsha() {
+    return (
+        <section>
+            <h1>Adarsha Regmi</h1>
+        </section>
+    )
+}
