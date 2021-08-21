21409f0a5d18e6c488b05c58836f471381e78623
Adarsha Regmi <adarsharegmi121@gmail.com>
2021-08-21 19:45:05 +0545

    add changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
+import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 
 
 export default function App() {
