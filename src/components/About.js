dee39219f1967efe99f7e95f21ca4428380cb2f5
adarsharegmi121@gmail.com <addy@Addys-MacBook-Air.local>
2024-01-10 00:24:43 +0545

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -11,20 +11,14 @@ export default function About() {
                     </h1>
                     <p className="mb-8 leading-relaxed">
                         Not Better than
-                        <a href="https://www.sarojpaudyal.com.np" onclick="window.open('www.sarojpaudyal.com.np', '_blank'); return false;">Saroj</a>
                         <a href="https://www.sarojpaudyal.com.np" target="_blank" rel="noopener noreferrer"> Saroj</a>
 
                     </p>
                     <div className="flex justify-center">
                         <h3
-                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                            Contact Me at adarsharegmi121@gmail.com
+                            className="inline-flex text-white bg-green-500 border-4 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                            Contact  at adarsharegmi121@gmail.com
                         </h3>
-                        <a
-                            href="#projects"
-                            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                            See My Past Work
-                        </a>
                     </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,16 +10,17 @@ export default function Navbar() {
                     </a>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-                    <a href="#projects" className="mr-5 hover:text-white">
+                    <a href="#projects" className="mr-5 hover:text-white hover:text-lg text-white">
                         Past Work
                     </a>
-                    <a href="#skills" className="mr-5 hover:text-white">
+                    <a href="#skills" className="mr-5 hover:text-white hover:text-lg text-white">
                         Skills
                     </a>
                 </nav>
                 <a
                     href="#contact"
-                    className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+                    className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 
+                    rounded text-base mt-4 md:mt-0 hover:text-lg text-white hover:text-white">
                     Consult with me and let's build something.
                 </a>
             </div>
